5d0ea96645529bc317a9445ce069f31bbe3e4eb9
Kentq360s <146793348+Kentq360s@users.noreply.github.com>
2023-10-13 21:05:10 +0800

    update

--- Frontend/src/pages/WelcomeScreen/SplashScreen/splashScreen.jsx
+++ Frontend/src/pages/WelcomeScreen/SplashScreen/splashScreen.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { Box, Stack, Typography } from "@mui/material";
-import { background, logo } from '../../../assets/Logo';
+  import React from 'react';
+  import { Box, Stack, Typography } from "@mui/material";
+  import { background, logo } from '../../../assets/Logo';
 
-const SplashScreen = () => {
-  return (
-    <Box
-      sx={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "white",
-      }}
-    >
-      <Stack textAlign="center" spacing={2} sx={{ display: "flex", flexDirection: "column", alignItems: "center", width: "50vmin" }}>
-        <Stack sx={{ width: "160px", height: "127px" }}>
-          <img src={logo} alt="Logo" />
+  const SplashScreen = () => {
+    return (
+      <Box
+        sx={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+        }}
+      >
+        <Stack textAlign="center" spacing={2} sx={{ display: "flex", flexDirection: "column", alignItems: "center", width: "50vmin" }}>
+          <Stack sx={{ width: "160px", height: "127px" }}>
+            <img src={logo} alt="Logo" />
+          </Stack>
+        <Stack >
+        <Typography variant="body1">   
+            Let us help you manage your financial.
+          </Typography>
         </Stack>
-       <Stack >
-       <Typography variant="body1">
-          Let us help you manage your financial.
-        </Typography>
-       </Stack>
-      </Stack>
-    </Box>
-  );
-}
+        </Stack>
+      </Box>
+    );
+  } 
 
-export default SplashScreen;
+  export default SplashScreen;
